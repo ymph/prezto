87318069230e132ccd8ec223ed812f70e0578391
Yves-Marie Haussonne <1218002+ymph@users.noreply.github.com>
2019-12-17 12:56:10 +0100

    update zpreztorc after prezto upgrade

---
+++ contrib/poetry/init.zsh
@@ -0,0 +1,15 @@
+#
+# Sets completion options.
+#
+# Authors:
+#   Robby Russell <robby@planetargon.com>
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Return if requirements are not found.
+if [[ "$TERM" == 'dumb' ]]; then
+  return 1
+fi
+
+# Add zsh-completions to $fpath.
+fpath=("${0:h}/functions" $fpath)
