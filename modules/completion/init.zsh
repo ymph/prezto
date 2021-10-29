34ddfb33baf2df895638c1cbffe048341e533822
Yves-Marie Haussonne <1218002+ymph@users.noreply.github.com>
2021-10-29 11:32:16 +0200

    add personal completion, docker + docker-compose

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -13,6 +13,7 @@ fi
 
 # Add zsh-completions to $fpath.
 fpath=(${0:h}/external/src $fpath)
+fpath=(${0:h}/personal/src $fpath)
 
 # Add completion for keg-only brewed curl when available.
 if (( $+commands[brew] )) \
